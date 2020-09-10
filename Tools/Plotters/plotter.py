e433d17a3ebbecd6e9bc5e0aae2b5a88b64072b2
Uzziel <uzzie.perez@cern.ch>
2020-09-10 11:14:17 -0500

    prettify

--- Tools/Plotters/plotter.py
+++ Tools/Plotters/plotter.py
@@ -31,11 +31,14 @@ def createRatio(h1, h2):
 
 # Compare genPho1pt with patPho1Pt
 c = TCanvas("c", "canvas", 500, 500)
-hgenpho1pt = createHist(f, kBlack, "genPho1Pt")
-hpatpho1pt = createHist(f, kRed, "patPho1Pt")
+hgenpho1pt = createHist(f, kOrange+8, "genPho1Pt")
+hpatpho1pt = createHist(f, kBlack, "patPho1Pt")
+
+hgenpho1pt.SetFillColor(kOrange-2)
+hpatpho1pt.SetMarkerStyle(3)
 
 hgenpho1pt.Draw("hist")
-hpatpho1pt.Draw("same, hist")
+hpatpho1pt.Draw("same")
 
 leg = TLegend(0.60, 0.75, 1.0, 0.85)
 leg.SetBorderSize(0)
@@ -43,12 +46,38 @@ leg.SetFillColor(0)
 leg.SetFillStyle(0)
 leg.SetTextFont(42)
 leg.SetTextSize(0.035)
-leg.AddEntry(hgenpho1pt, "genPho1", "l")
-leg.AddEntry(hpatpho1pt, "patPho1", "l")
+leg.AddEntry(hgenpho1pt, "genPho1", "f")
+leg.AddEntry(hpatpho1pt, "patPho1", "ep")
 leg.Draw()
 
 
 c.Draw()
 c.SaveAs("plots/GenVsPatPho1pt.pdf")
-# Compare
-# hpatpho1ptLoose = createHist(f, kBlue, "patPho1PtLoose")
+
+# Compare patPho1 with those that passed Loose
+cLoose = TCanvas("cLoose", "canvasLoose", 500, 500)
+
+hpatpho1ptLoose = createHist(f, kBlack, "patPho1PtLoose")
+
+hpatpho1pt.SetLineColor(kOrange+8)
+hpatpho1pt.SetMarkerStyle(1)
+hpatpho1pt.SetFillColor(kOrange-2)
+hpatpho1pt.SetFillStyle(3144)
+hpatpho1ptLoose.SetFillColor(kCyan-9)
+hpatpho1ptLoose.SetFillStyle(3144)
+
+hpatpho1pt.Draw("hist")
+hpatpho1ptLoose.Draw("same, hist")
+
+l = TLegend(0.60, 0.75, 1.0, 0.85)
+l.SetBorderSize(0)
+l.SetFillColor(0)
+l.SetFillStyle(0)
+l.SetTextFont(42)
+l.SetTextSize(0.035)
+l.AddEntry(hpatpho1pt, "No ID", "f")
+l.AddEntry(hpatpho1ptLoose, "Loose ID", "f")
+l.Draw()
+
+cLoose.Draw()
+cLoose.SaveAs("plots/LoosePhoEff.pdf")
